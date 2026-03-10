1ea78c1fb61798a3a7927997b9cbfa482fede29f
ERBS-1986 <eudesrbsantos@gmail.com>
2026-03-10 14:30:34 +0000

    30 - ATT para Android Studio

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dailytask.app',
+  appName: 'Daily Task',
+  webDir: 'dist'
+};
+
+export default config;
